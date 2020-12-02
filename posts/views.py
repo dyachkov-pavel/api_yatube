ac104d956f28a1d416528db11a49f9f6614b8272
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-12-02 15:28:15 +0300

    perform_create+get_queryset

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status, permissions
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwner
@@ -12,12 +10,8 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
-    def create(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -25,16 +19,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
-    def list(self, request, *args, **kwargs):
+    def get_queryset(self, *args, **kwargs):
         post_id = self.kwargs['post_id']
-        post = get_object_or_404(Post, id=post_id)
-        queryset = post.comments.all()
-        serializer = CommentSerializer(queryset, many=True)
-        return Response(serializer.data)
+        post = get_object_or_404(Post, id=post_id) # проверка наличия поста в БД
+        return post.comments.all()
 
-    def create(self, request, *args, **kwargs):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer, *args, **kwargs):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(Post, id=post_id) # проверка наличия поста в БД
+        serializer.save(author=self.request.user)
